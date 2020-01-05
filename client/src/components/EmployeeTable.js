5c01fdacfef25b7aa5dfc27a097d3c36a7e420b8
jackhyeonwoo <jack.hw8@gmail.com>
2020-01-04 18:43:51 -0800

    table fix

--- client/src/components/EmployeeTable.js
+++ client/src/components/EmployeeTable.js
@@ -3,29 +3,27 @@ import EmployeeTableRow from './EmployeeTableRow';
 
 const EmployeeTable = (props) => {
     return(
-        <div className="container">
-            <table className="table">
-                <thead>
-                    <tr>
-                    {
-                        // <th scope="col">#</th> 
-                    }
-                    <th scope="col">First Name</th>
-                    <th scope="col">Last Name</th>
-                    <th scope="col">Job Title</th>
-                    <th scope="col">Salary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {props.employees.map(employee=>{
-                        return <EmployeeTableRow key={employee._id}
-                                                employee={employee}
-                                                deleteHandler={props.deleteHandler}
-                                                showEditForm={props.showEditForm}/>
-                    })}
-                </tbody>
-            </table>
-        </div>
+        <table className="table">
+            <thead>
+                <tr>
+                {
+                    // <th scope="col">#</th> 
+                }
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Job Title</th>
+                <th scope="col">Salary</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.employees.map(employee=>{
+                    return <EmployeeTableRow key={employee._id}
+                                            employee={employee}
+                                            deleteHandler={props.deleteHandler}
+                                            showEditForm={props.showEditForm}/>
+                })}
+            </tbody>
+        </table>
     )
 }
 
--- client/src/components/EmployeeTableRow.js
+++ client/src/components/EmployeeTableRow.js
@@ -3,29 +3,25 @@ import React from 'react';
 const EmployeeTableRow = (props) => {
     const {firstName, lastName, job, salary, _id} = props.employee;
     return(
-        <div className="container">
-            <tr>
-                {
-                    // <th scope="row">{_id}</th>
-                    // <td>{firstName}</td>
-                    // <td>{lastName}</td>
-                    // <td>{job}</td>
-                    // <td>{salary}</td>
-                }
-
-                <td scope="row">{firstName}</td>
-                <td>{lastName}</td>
-                <td>{job}</td>
-                <td>{salary}</td>
-
-                <td>
-                    <div class="btn-group" role="group" aria-label="Basic example">
-                        <button type="button" onClick={props.showEditForm.bind(this, props.employee)} className="btn btn-secondary">Edit</button>
-                        <button type="button" onClick={props.deleteHandler.bind(this, _id)} className="btn btn-danger">Delete</button>
-                    </div>
-                </td>
-            </tr>
-        </div>
+        <tr>
+            {
+                // <th scope="row">{_id}</th>
+                // <td>{firstName}</td>
+                // <td>{lastName}</td>
+                // <td>{job}</td>
+                // <td>{salary}</td>
+            }
+            <th scope="row">{firstName}</th>
+            <td>{lastName}</td>
+            <td>{job}</td>
+            <td>{salary}</td>
+            <td>
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" onClick={props.showEditForm.bind(this, props.employee)} className="btn btn-secondary">Edit</button>
+                    <button type="button" onClick={props.deleteHandler.bind(this, _id)} className="btn btn-danger">Delete</button>
+                </div>
+            </td>
+        </tr>
     )
 }
 
