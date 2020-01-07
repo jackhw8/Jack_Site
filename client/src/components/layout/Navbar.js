6dda5b8a0ca2f366b8ce245f774a204f0e235be7
jackhyeonwoo <jack.hw8@gmail.com>
2020-01-07 02:35:50 -0800

    Change navbar font

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-light mainNav">
+        <nav className="navbar navbar-expand-sm navbar-light" style={{'fontFamily': 'Bitter, sans-serif'}}>
             <a href="/" className="navbar-brand">Jack Kim</a>
             <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
                 <span className="navbar-toggler-icon"></span>
