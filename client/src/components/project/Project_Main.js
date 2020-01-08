271dae98230719fd01c3ba4e2e45669f4369e150
jackhyeonwoo <jack.hw8@gmail.com>
2020-01-08 02:57:16 -0800

    Change project again

--- client/src/components/project/Project_Main.js
+++ client/src/components/project/Project_Main.js
@@ -17,7 +17,7 @@ const Project_Main = () => {
 
         <main className="container" style={{'maxWidth': '40em'}}>
             {/* Ezian or Mcomics */}
-            <div className="row clearfix border-bottom border-light-gray circle-line py3"  style={{'paddingTop':'30px'}}>
+            <div className="row mxn2 clearfix mb2 py3 border-bottom border-light-gray circle-line"  style={{'paddingTop':'30px'}}>
                 {/* mcomics pic */}
                 <div className="col sm-col-4 px2 div1">
                     <a href="https://www.mcomics.co.kr/" target="_blank" rel="noopener noreferrer">
@@ -40,9 +40,26 @@ const Project_Main = () => {
             </div>
 
             {/* Python Auto-receipt */}
-            <div>
-
-
+            <div className="row mxn2 clearfix mb2 py3 border-bottom border-light-gray circle-line"  style={{'paddingTop':'30px'}}>
+                {/* mcomics pic */}
+                <div className="col sm-col-4 px2 div1">
+                    <a href="https://www.mcomics.co.kr/" target="_blank" rel="noopener noreferrer">
+                        <img src="/pic/mcomics.PNG" alt="mcomics" className="i2" style={{'width':'350px', 'height':'350px'}}></img>
+                    </a>
+                </div>
+                {/* mcomics explanation */}
+                <div className="col sm-col-8 px2 mb2 div2">
+                    <a href="https://www.mcomics.co.kr/" className="a2" target="_blank" rel="noopener noreferrer">
+                        <h2>Mcomics</h2>
+                    </a>
+                    <a href="https://www.mcomics.co.kr/" className="a2 mb2" target="_blank" rel="noopener noreferrer" style={{'fontSize':'0.75rem', 'display':'block', 'color': '#0096cc'}}>
+                        https://www.mcomics.co.kr/
+                    </a>
+                    <p style={{'textAlign': 'left'}}>
+                        Mcomics is a website of web comics and graphic novels. I mostly worked on the frontend for this.
+                        I also increased ad banner so that users can giveaways and popularity polls
+                    </p>
+                </div>
             </div>
 
             {/* CRUD */}
